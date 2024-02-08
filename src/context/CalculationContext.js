11351030992efd7570f2f05eca5f42d478a4183b
OlegAleksashov <fairp88@tut.by>
2024-02-08 17:52:21 +0300

    rewritten handleOperations

--- src/context/CalculationContext.js
+++ src/context/CalculationContext.js
@@ -1,53 +1,111 @@
-import { createContext, useState } from "react"
-import React from 'react'
+import { createContext, useState } from "react";
+import React from "react";
 
 export const CalculationContext = createContext();
 
-export const CalculationProvider = ({children}) => {
-  const [result, setResult] = useState([]);
-  const [selectOperator, setSelectOperator] = useState([])
+export const CalculationProvider = ({ children }) => {
+  const [result, setResult] = useState("");
 
-  // const handleOperations = () => {
-  //   let array = [];
-  //   let arrayResult = [];
-  //   array = result.toString();
-  //   if (array.includes('/')) {
-  //     arrayResult = array.split('/');
-  //     setResult(arrayResult[0] / arrayResult[1]);
-  //   } else if (array.includes('*')) {
-  //     arrayResult = array.split('*');
-  //     setResult(arrayResult[0] * arrayResult[1]);
-  //   } else if (array.includes('+')) {
-  //     arrayResult = array.split('+');
-  //     setResult(+arrayResult[0] + +arrayResult[1]);
-  //   } else if (array.includes('-')) {
-  //     arrayResult = array.split('-');
-  //     setResult(arrayResult[0] - arrayResult[1]);
-  //   }
-  // };
+  function handleOperations() {
+    const operators = {
+      "+": { precedence: 1, associativity: "left" },
+      "-": { precedence: 1, associativity: "left" },
+      "*": { precedence: 2, associativity: "left" },
+      "/": { precedence: 2, associativity: "left" },
+    };
 
-  const handleOperations = () => {
-     let array = [];
-     array = result.toString();
-     // eslint-disable-next-line no-eval
-     setResult(eval(array))
+    function isOperator(token) {
+      return token in operators;
+    }
+
+    function getPrecedence(operator) {
+      return operators[operator].precedence;
+    }
+
+    function applyOperator(operator, operand1, operand2) {
+      switch (operator) {
+        case "+":
+          return operand1 + operand2;
+        case "-":
+          return operand1 - operand2;
+        case "*":
+          return operand1 * operand2;
+        case "/":
+          return operand1 / operand2;
+        default:
+          console.log(`Sorry, we are out of ${operator}.`);
+      }
+    }
+
+    function evaluateExpression(tokens) {
+      const outputQueue = [];
+      const operatorStack = [];
+
+      for (const token of tokens) {
+        if (!isNaN(token)) {
+          outputQueue.push(parseFloat(token));
+        } else if (isOperator(token)) {
+          const currentOperator = token;
+          let topOperator = operatorStack[operatorStack.length - 1];
+
+          while (
+            isOperator(topOperator) &&
+            ((operators[currentOperator].associativity === "left" &&
+              getPrecedence(currentOperator) <= getPrecedence(topOperator)) ||
+              (operators[currentOperator].associativity === "right" &&
+                getPrecedence(currentOperator) < getPrecedence(topOperator)))
+          ) {
+            outputQueue.push(operatorStack.pop());
+            topOperator = operatorStack[operatorStack.length - 1];
+          }
+
+          operatorStack.push(currentOperator);
+        }
+      }
+
+      while (operatorStack.length > 0) {
+        outputQueue.push(operatorStack.pop());
+      }
+
+      const evaluationStack = [];
+
+      for (const token of outputQueue) {
+        if (!isNaN(token)) {
+          evaluationStack.push(token);
+        } else if (isOperator(token)) {
+          const operand2 = evaluationStack.pop();
+          const operand1 = evaluationStack.pop();
+          evaluationStack.push(applyOperator(token, operand1, operand2));
+        }
+      }
+
+      return evaluationStack[0];
+    }
+
+    const tokens = result.match(/(\d+(\.\d+)?|\S)/g);
+    setResult(evaluateExpression(tokens));
   }
-  
+
   const handlePercentage = () => {
     setResult(result / 100);
-  }
+  };
 
   const handleDeleteOne = () => {
-    setResult(result.slice(0, result.length - 1));
-  }
+    console.log(result);
+    if (typeof result == "number") {
+      setResult("");
+    } else {
+      setResult(result.slice(0, result.length - 1));
+    }
+  };
 
   const handleDelete = () => {
-    setResult('');
-  }
+    setResult("");
+  };
 
   const handleDrawNumber = (input) => {
-    const operators = ['/', '*', '+', '-', '%'];
-  
+    const operators = ["/", "*", "+", "-", "%"];
+
     if (operators.includes(input)) {
       if (operators.includes(result[result.length - 1])) {
         setResult(result.slice(0, result.length - 1) + input);
@@ -59,23 +117,19 @@ export const CalculationProvider = ({children}) => {
     }
   };
 
-
   return (
     <CalculationContext.Provider
       value={{
         handleDeleteOne,
-        handleDrawNumber, 
+        handleDrawNumber,
         handleDelete,
         handlePercentage,
         result,
         handleOperations,
-        selectOperator,
-        setSelectOperator,
-        setResult
+        setResult,
       }}
     >
       {children}
     </CalculationContext.Provider>
-  )
-}
-
+  );
+};
